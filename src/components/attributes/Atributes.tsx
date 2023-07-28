0f8a163ff51da0fe4ade4e5b8497c1af947d6652
John Ayling <johnayling@yahoo.com>
2023-07-28 11:00:18 -0500

    Ad SFC credits.

--- src/components/attributes/Atributes.tsx
+++ src/components/attributes/Atributes.tsx
@@ -21,7 +21,7 @@ function Atributes() {
         Credits
       </div>
       <div>
-        Balloon Font: <a href="https://www.fontspace.com/ji-solid-balloon-caps-font-f5466">Solid Balloon Caps</a>
+        <div>Balloon Font: <a href="https://www.fontspace.com/ji-solid-balloon-caps-font-f5466">Solid Balloon Caps</a></div>
       </div>
       <div>
         Balloons: <a href="https://www.vecteezy.com/vector-art/5007767-shiny-colorful-balloons-set-isolate">Cartoon Balloon Vectors by Vecteezy</a>
@@ -33,28 +33,55 @@ function Atributes() {
         Music:
       </div>
       <div>
-        "Balloon Game" Kevin MacLeod (incompetech.com)
+        <div>"Balloon Game" Kevin MacLeod <a href="https://incompetech.com/music/royalty-free/music.html" target="_blank" rel="noreferrer">incompetech.com</a></div>
         <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
       </div>
 
       <div>
-        "Newer Wave" Kevin MacLeod (incompetech.com)
+        <div>"Newer Wave" Kevin MacLeod <a href="https://incompetech.com/music/royalty-free/music.html" target="_blank" rel="noreferrer">incompetech.com</a></div>
         <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
       </div>
       <div>
-        "Robobozo" Kevin MacLeod (incompetech.com)
+        <div>"Robobozo" Kevin MacLeod <a href="https://incompetech.com/music/royalty-free/music.html" target="_blank" rel="noreferrer">incompetech.com</a></div>
         <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
       </div>
       <div>
-        "Raving Energy" Kevin MacLeod (incompetech.com)
+        <div>"Raving Energy" Kevin MacLeod <a href="https://incompetech.com/music/royalty-free/music.html" target="_blank" rel="noreferrer">incompetech.com</a></div>
         <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
       </div>
       <div>
-        "Balloon Game" Kevin MacLeod (incompetech.com)
+        <div>"Balloon Game" Kevin MacLeod <a href="https://incompetech.com/music/royalty-free/music.html" target="_blank" rel="noreferrer">incompetech.com</a></div>
         <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
       </div>
       <div>
-        "Nowhere Land" Kevin MacLeod (incompetech.com)
+        <div>"Nowhere Land" Kevin MacLeod <a href="https://incompetech.com/music/royalty-free/music.html" target="_blank" rel="noreferrer">incompetech.com</a></div>
+        <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
+      </div>
+      <div className={styles.music}>
+        SFX:
+      </div>
+      <div>
+        <div>"Balloon Pop" Aesterial-Arts <a target="_blank" rel="noreferrer" href="https://freesound.org/people/Aesterial-Arts/sounds/633835/">freesound.org</a></div>
+        <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
+      </div>
+      <div>
+        <div>"Balloon Boy.20" crabhead <a target="_blank" rel="noreferrer" href="https://freesound.org/people/crabhead/sounds/265228/">freesound.org</a></div>
+        <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
+      </div>
+      <div>
+        <div>"Beeps 18" Greencouch <a target="_blank" rel="noreferrer" href="https://freesound.org/people/Greencouch/sounds/124900/">freesound.org</a></div>
+        <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
+      </div>
+      <div>
+        <div>"AI, No" MATRIXXX_ <a target="_blank" rel="noreferrer" href="https://freesound.org/people/MATRIXXX_/sounds/453137/">freesound.org</a></div>
+        <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
+      </div>
+      <div>
+        <div>"Beep 3" anthonychartier2020 <a target="_blank" rel="noreferrer" href="https://freesound.org/people/anthonychartier2020/sounds/560188/">freesound.org</a></div>
+        <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
+      </div>
+      <div>
+        <div>"UI Confirmation Alert, C3.wav" InspectorJ <a target="_blank" rel="noreferrer" href="https://freesound.org/people/InspectorJ/sounds/403017/">freesound.org</a></div>
         <a href="http://creativecommons.org/licenses/by/4.0/">Licensed under Creative Commons: By Attribution 4.0 License</a>
       </div>
     </div>
